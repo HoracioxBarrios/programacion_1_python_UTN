f1151060364f3cefbb8b9b8af8d82a73c6162656
horacio barrios <horacioxbarrios@gmail.com>
2023-03-31 03:26:30 -0300

    Practicas

--- 1-Ejercicios-If/4_ejercicio.py
+++ 1-Ejercicios-If/4_ejercicio.py
@@ -1,9 +1,9 @@
 '''
 4- Escribir un programa que le pida al usuario que ingrese 
 dos números enteros, y luego imprima "El primer número es mayor"
- si el primer número es mayor que el segundo, "El segundo número es mayor"
- si el segundo número es mayor que el primero, o "Los dos números son iguales" 
- si los dos números son iguales.
+si el primer número es mayor que el segundo, "El segundo número es mayor"
+si el segundo número es mayor que el primero, o "Los dos números son iguales" 
+si los dos números son iguales.
 '''
 
 primer_numero_ingresado_str = input("Ingrese un numero")
