3c200d57fef751935832b60d2772a18368c1168b
horacio barrios <horacioxbarrios@gmail.com>
2023-04-15 08:34:27 -0300

    Ejercicio del video clase3

---
+++ 0_Clases_semanales/3_0_clase.preparado_ej_clase3.py
@@ -0,0 +1,31 @@
+while (True):
+    respuesta_str = input(
+        "Ingrese 1- Tema mas visto\n 2- Tema menos Visto\n 3- Tema mas Largo\n 4- Tema mas corto \n 5- Duracion promedio de Temas\n 6- Promedio de Vistas\n 7- Mostrar Lista\n 8- Salir")
+    respuesta_int = int(respuesta_str)
+    
+    match(respuesta_int):
+        case 1:
+            #Tema mas visto
+            pass
+        case 2:
+            #Tema menos visto
+            pass
+        case 3:
+            #Tema mas largo
+            pass
+        case 4:
+            #Tema mas corto
+            pass
+        case 5:
+            #Duracion Promedio de temas
+            pass
+        case 6:
+            #Promedio de vistas
+            pass
+        case 7:
+            #Mostrar Lista
+            pass
+        case 8:
+            #Salir
+            pass
+    input("Pulse enter para continuar")
