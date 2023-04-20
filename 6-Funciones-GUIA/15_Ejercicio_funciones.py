66a38b00ee3db43e628d082e3d9aea304a7394fc
horacioxbarrios <horacioxbarrios@gmail.com>
2023-04-20 19:41:42 -0300

    Ejercicios Funciones

--- 6-Funciones-GUIA/15_Ejercicio_funciones.py
+++ 6-Funciones-GUIA/15_Ejercicio_funciones.py
@@ -9,31 +9,87 @@ lista_de_peliculas = [{"titulo": "El Señor de los anillos", "genero": "Fantasia
                     {"titulo": "Terminator","genero": "Ciencia Ficcion", "Director": "James Cameron"},
                     {"titulo": "El Señor de los anillos 2 ", "genero": "Fantasia", "Director": "Peter Jackson"}]
 
-def buscar_peliculas_por_genero(lista):
+def calcular_por_genero(lista,clave):
     '''
-    contabiliza peliculas segun su genero
-    recibe una lista de diccionario peliculas
-    retorna un nuevo diccionario con los generos y su cantidad
+    de una lista cuenta, cuenta los elementos que se repiten
+    recibe una lista
+    retorna un diccionario con los elementos contabilizados
     '''
-    contador_Fantasia = 0
-    contador_ciencia_ficcion = 0
-    contador_aventura = 0
-    dicc_conteo_pelicula_por_generos = {}
-    
-    for pelicula in lista:
-        if(pelicula["genero"] == "Fantasia"):
-            contador_Fantasia += 1
-            dicc_conteo_pelicula_por_generos["Fantasia"] = contador_Fantasia
-            
-        if(pelicula["genero"] == "Aventura"):
-            contador_aventura += 1
-            dicc_conteo_pelicula_por_generos["Aventura"] = contador_aventura
-            
-        if(pelicula["genero"] == "Ciencia Ficcion"):
-            contador_ciencia_ficcion += 1
-            dicc_conteo_pelicula_por_generos["Ciencia Ficcion"] = contador_ciencia_ficcion
-            
-    return dicc_conteo_pelicula_por_generos
+    dicc_contador_peliculas = {}
 
-print("Las peliculas por genero son: {0}".format(
-    buscar_peliculas_por_genero(lista_de_peliculas)))
+    for elemento in lista:
+        
+        if elemento[clave] in dicc_contador_peliculas:
+            dicc_contador_peliculas[elemento[clave]] += 1
+        else:
+            dicc_contador_peliculas[elemento[clave]] = 1
+    return dicc_contador_peliculas
+
+resultado = calcular_por_genero(lista_de_peliculas,"genero")
+print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+# def calcular_peliculas_por_genero(lista_peliculas):
+#     dicc_contador_peliculas = {}
+
+#     for pelicula in lista_peliculas:
+#         clave = pelicula["genero"]
+#         if clave in dicc_contador_peliculas:
+#             dicc_contador_peliculas[clave] += 1
+#         else:
+#             dicc_contador_peliculas[clave] = 1
+#     return dicc_contador_peliculas
+
+# resultado = calcular_peliculas_por_genero(lista_de_peliculas)
+# print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def buscar_peliculas_por_genero(lista):
+#     '''
+#     contabiliza peliculas segun su genero
+#     recibe una lista de diccionario peliculas
+#     retorna un nuevo diccionario con los generos y su cantidad
+#     '''
+#     contador_Fantasia = 0
+#     contador_ciencia_ficcion = 0
+#     contador_aventura = 0
+#     dicc_conteo_pelicula_por_generos = {}
+    
+#     for pelicula in lista:
+#         if(pelicula["genero"] == "Fantasia"):
+#             contador_Fantasia += 1
+#             dicc_conteo_pelicula_por_generos["Fantasia"] = contador_Fantasia
+            
+#         if(pelicula["genero"] == "Aventura"):
+#             contador_aventura += 1
+#             dicc_conteo_pelicula_por_generos["Aventura"] = contador_aventura
+            
+#         if(pelicula["genero"] == "Ciencia Ficcion"):
+#             contador_ciencia_ficcion += 1
+#             dicc_conteo_pelicula_por_generos["Ciencia Ficcion"] = contador_ciencia_ficcion
+            
+#     return dicc_conteo_pelicula_por_generos
+
+# print("Las peliculas por genero son: {0}".format(
+#     buscar_peliculas_por_genero(lista_de_peliculas)))
