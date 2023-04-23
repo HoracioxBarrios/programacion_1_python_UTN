555452226b6969210f25910bc2aa8d86e7bded42
horacio barrios <horacioxbarrios@gmail.com>
2023-04-23 10:59:24 -0300

    Ejercicio

--- 0_Clases_Apoyo/0Ejercicios_Clase_de_apoyo/22-04Ejercicio con Menú de Opciones.py
+++ 0_Clases_Apoyo/0Ejercicios_Clase_de_apoyo/22-04Ejercicio con Menú de Opciones.py
@@ -3,7 +3,8 @@ Ejercicio con Menú de Opciones
 Realizar un programa utilizando los conceptos de la clase: 
 Opciones del menú: 
 
-# 1 Cargar una lista con 10 nombres de animales (perro, gato, león, etc,) y de que tipo son (terrestre, anfibio, volador). 
+# 1 Cargar una lista con 10 nombres de animales (perro, gato, león, etc,) 
+y de que tipo son (terrestre, anfibio, volador). 
 
 # 2 Imprimir la lista completa de animales con su tipo. 
 
@@ -15,9 +16,77 @@ Opciones del menú:
 
 # 6 Pedir un animal e informar si está en la lista y sus datos correspondientes si efectivamente está en la lista. 
 
-# 7 Pedir un animal e informar la primer ocurrencia de ese animal en la lista si es que existe. # 8 Informar la cantidad de animales por cada tipo de animal. 
+# 7 Pedir un animal e informar la primer ocurrencia de ese animal en la lista si es que existe. 
 
-# 9 Vaciar la lista. 
-#10 Salir.
+# 8 Vaciar la lista. 
+# 9 Informar la cantidad de animales por cada tipo de animal. 
+# 10 Salir.
 
 '''
+# lista_de_animales = [
+#     {"animal": "leon", "tipo": "terrestre"},
+#     {"animal": "tortuga", "tipo": "terrestre"},
+#     {"animal": "ballena", "tipo": "acuatico"},
+#     {"animal": "delfin", "tipo": "acuatico"},
+#     {"animal": "rana", "tipo": "anfibio"},
+#     {"animal": "aguila", "tipo": "volador"},
+#     {"animal": "tigre", "tipo": "terrestre"},
+#     {"animal": "gato", "tipo": "terrestre"},
+#     {"animal": "perro", "tipo": "terrestre"},
+#     {"animal": "paloma", "tipo": "volador"},]
+
+
+def agregar_a_lista(nombre : str, tipo : str, cantidad :int)-> list[dict]:
+    ''' 
+    toma nombres y tipos de animal de un input y crea una lista de dicc animales.
+    recibe nombre, tipo, y cantidad a ingresar.
+    devuelve una lista de dicc
+    '''
+    lista = []
+    contador = cantidad
+    while(contador > 0):
+        if(nombre not in lista):
+            dicc_animal = {}
+            dicc_animal["animal"] = nombre
+            dicc_animal["tipo"] = tipo
+            lista.append(dicc_animal)
+        contador -= 1
+    return lista
+
+def print_animal(lista):
+    for elem in lista:
+        print("Animal: {0} su tipo: {1}".format(elem["animal"], elem["tipo"]))
+        
+        
+        
+while (True):
+    respuesta_str = input(
+        "Elija una opcion:\n 1-Ingreser animal\n 2-ver lista completa\n 3-ver porcentaje por tipo\n 4-Mayor cantidad de animales por tipo\n 5-Menor cantidad de animales por tipo \n 6-Buscar informacion\n 7-Buscar si existe\n 8-vaciar lista\n 9-cantidad Animales por tipo\n 10-Salir\n")
+    respuesta_int = int(respuesta_str)
+    match(respuesta_int):
+        case 1:
+                nombre_animal = input("Ingrese animal")
+                tipo_animal = input("Ingrese tipo de animal")
+                cantidad_a_ingresar = 1
+                lista_de_animales = agregar_a_lista(nombre_animal, tipo_animal, cantidad_a_ingresar)
+                print(lista_de_animales)
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            pass
+        case 8:
+            pass
+        case 9:
+            pass
+        case 10:
+            break
+    input("Enter para continuar ")
+        
--- 0_Clases_Apoyo/0Ejercicios_Clase_de_apoyo/22-04EjerciciosCadenas.py
+++ 0_Clases_Apoyo/0Ejercicios_Clase_de_apoyo/22-04EjerciciosCadenas.py
@@ -1,41 +1,23 @@
-'''
-1.Contar letras: Crea una función que tome una cadena de texto como argumento y 
-cuente el número de letras que contiene.
-
-2.Eliminar caracteres: Crea una función que tome una cadena de texto y un carácter como argumentos, y elimine todas las ocurrencias de ese carácter en la cadena.
-
-3.Contar palabras: Crea una función que tome una cadena de texto como argumento y 
-cuente el número de palabras que contiene. 
-Suponga que las palabras están separadas por un espacio.
-
-4.Reemplazar palabras: Crea una función que tome una cadena de texto, una palabra y otra palabra como argumentos, y reemplace todas las ocurrencias de la primera palabra por la segunda en la cadena.
-
-5.Buscar patrón: Crea una función que tome dos cadenas de texto como argumentos: una cadena principal y un patrón. La función debe encontrar todas las ocurrencias del patrón en la cadena principal y devolver una lista con las posiciones donde se encontró el patrón.
-
-'''
-
-
-
-
-
+#Ejercicios_ Cadenas 22-04-2023
 '''
 1.Contar letras: Crea una función que tome una cadena de texto como argumento y 
 cuente el número de letras que contiene.
 '''
-# palabra_ingresada = input("Ingrese una Palabra\n")
+palabra_ingresada = input("Ingrese una Palabra\n")
 
-# def contar_letra(cadena : str)-> int:
-#     '''
-#     cuenta una cadena de caracteres
-#     recibe una cadena
-#     devuelve la cantidad de caracteres
-#     '''
-#     largo_de_cadena = len(cadena)
-#     return largo_de_cadena
+def contar_letra(cadena : str)-> int:
+    '''
+    cuenta una cadena de caracteres
+    recibe una cadena
+    devuelve la cantidad de caracteres
+    '''
+    largo_de_cadena = len(cadena)
+    return largo_de_cadena
 
-# largo_de_la_palabra = contar_str(palabra_ingresada)
+largo_de_la_palabra = contar_letra(palabra_ingresada)
 
-# print("La Cantidad de caracteres es: {0}".format(largo_de_la_palabra))
+print("La Cantidad de caracteres es: {0}".format(largo_de_la_palabra))
+
 
 
 
@@ -43,42 +25,51 @@ cuente el número de letras que contiene.
 2.Eliminar caracteres: Crea una función que tome una cadena de texto y un carácter 
 como argumentos, y elimine todas las ocurrencias de ese carácter en la cadena.
 '''
-# palabra_ingresada = input("Ingrese una Palabra\n")
-# letra_a_borrar = input("Que letra queres borrar?\n")
+palabra_ingresada = input("Ingrese una Palabra\n")
+letra_a_borrar = input("Que letra queres borrar?\n")
 
-# def borrar_str(cadena : str, caracter : str):
-#     nueva_cadena = cadena.replace(caracter, "")
+def borrar_str(cadena : str, caracter : str)-> str:
+    '''
+    De una cadena de texto, borra caracteres segun el caracter ingresado
+    recibe arg(1) una cadena a comprobar, y arg(2) el caracter buscado
+    devuelve una nueva cadena
+    '''
+    nueva_cadena = cadena.replace(caracter, "")
     
-#     return nueva_cadena
+    return nueva_cadena
 
-# nueva_palabra = borrar_str(palabra_ingresada, letra_a_borrar)
+nueva_palabra = borrar_str(palabra_ingresada, letra_a_borrar)
 
-# print("La nueva palabra es: {0}".format(nueva_palabra))
+print("La nueva palabra es: {0}".format(nueva_palabra))
+
+
+
+
 
 '''
 3.Contar palabras: Crea una función que tome una cadena de texto como argumento y 
 cuente el número de palabras que contiene. 
 Suponga que las palabras están separadas por un espacio.
 '''
-# palabras = input("Ingrese una Palabra\n")
+palabras = input("Ingrese una Palabra\n")
 
-# def contar_letras(cadena : str)-> int:
-#     '''
-#     cuenta una cadena de caracteres
-#     recibe una cadena
-#     devuelve la cantidad de caracteres
-#     '''
-#     contador_de_caracteres = 0
-#     for letra in cadena:
-#         if(letra != " "):
-#             contador_de_caracteres += 1
+def contar_letras(cadena : str)-> int:
+    '''
+    cuenta una cadena de caracteres
+    recibe una cadena
+    devuelve la cantidad de caracteres
+    '''
+    contador_de_caracteres = 0
+    for letra in cadena:
+        if(letra != " "):
+            contador_de_caracteres += 1
         
-#     return contador_de_caracteres
+    return contador_de_caracteres
 
-# largo_de_cadena = contar_letras(palabras)
+largo_de_cadena = contar_letras(palabras)
 
 
-# print("La Cantidad de caracteres es: {0}".format(largo_de_cadena))
+print("La Cantidad de caracteres es: {0}".format(largo_de_cadena))
 
 
 ''' 
@@ -87,8 +78,50 @@ una palabra y otra palabra como argumentos, y reemplace todas las
 ocurrencias de la primera palabra por la segunda en la cadena.
 '''
 
-cadena = "Hola, le traje el Perro"
+frase = "Hola, le traje el Perro"
 palabra_buscada = "Perro"
 palabra_a_reemplazar = "Gato"
+
+def reemplazar_palabras(frase, palabra_origen, palabra_destino):
+    '''
+    reemplaza una palabra por otra 
+    recibe un cadena de texto: ej: una frase
+    devuelve una cadena con la palabra reemplazada
+    '''
+    frase.replace(palabra_origen, palabra_destino)
+    return frase
+
+
+nueva_frase = reemplazar_palabras(frase, palabra_buscada, palabra_a_reemplazar)
+print(nueva_frase)
+
+'''
+5.Buscar patrón: Crea una función que tome dos cadenas de texto como argumentos
+: una cadena principal y un patrón. La función debe encontrar todas las 
+ocurrencias del patrón en la cadena principal y devolver una lista con las
+posiciones donde se encontró el patrón.
+
+'''
+frase = "Hola, aca le traje el perro"
+patron = "perro"
+
+def buscar_patron(cadena, patron):
+    '''
+    busca un patron, en una cadena de texto
+    recibe dos argunmentos: una cadena principal, y una cadena secundaria que se va a buscar
+    returna una lista con la posicion en donde se encontro un patron o igualdad.
+    '''
+    posiciones = []
+    indice = 0
+    while indice < len(cadena):
+        subindice = cadena.find(patron, indice)
+        if subindice == -1:
+            break
+        posiciones.append(subindice)
+        indice = subindice + len(patron)
+    return posiciones
+
+resultado = buscar_patron(frase, patron)
+print(resultado)
 
 
