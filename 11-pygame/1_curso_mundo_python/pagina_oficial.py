0ee1726cdd44072b28425cb93abf54cf4f85e0e0
horacio barrios <horacioxbarrios@gmail.com>
2023-06-07 21:50:17 -0300

    Pygame

--- 11-pygame/1_curso_mundo_python/pagina_oficial.py
+++ 11-pygame/1_curso_mundo_python/pagina_oficial.py
@@ -1,1 +1,6 @@
 #https://www.pygame.org/docs/ref/draw.html
+
+
+#recurso video https://stackoverflow-com.translate.goog/questions/62870381/how-to-play-video-in-pygame-currently?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=es&_x_tr_pto=sc
+
+#https://www.youtube.com/watch?v=Xu8SLkvFq8I
---
+++ 11-pygame/1_curso_mundo_python/practica6_mouse.py
@@ -0,0 +1,5 @@
+import pygame, sys
+pygame.init()
+
+negro = (0, 0, 0)
+blanco = (255, 255, 255)
